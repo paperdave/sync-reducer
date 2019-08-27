c3aa3a4552bc5b11b0fece778aa0b46ceed46b93
Dave Caruso <dave@davecode.me>
2019-08-27 17:25:25 -0400

    2.0.3: Ignore + TransformFunction

--- index.ts
+++ index.ts
@@ -1,29 +1,51 @@
 type Reducer<State, Args extends any[]> = (state: State, ...args: Args) => State;
 
+interface SyncReducerOptions<State> {
+  /** Optionally use a different storage than localStorage */
+  storage?: Storage;
+  /** Will write undefined for the given keys. Cannot be set with `transformFunction` */
+  ignore?: (keyof State)[];
+  /** If set, this will be called to transform the state into something else
+    * before it is set into the Storage. Cannot be set with `ignore` */
+  transformFunction?: (state: State) => State;
+};
+
 /**
  * Creates a reducer with the same properties of the passed reducer, but syncs it's value to
  * a Storage like localStorage
  *
  * @param reducer The reducer that you want to sync to localStorage
  * @param storageKey The key on localStorage that is used for syncing
- * @param storage Optionally use a different storage than localStorage
+ * @param options Options for the syncing
  */
 export default function syncReducer<State extends {}, Args extends any[]>(
   reducer: Reducer<State, Args>,
   storageKey: string,
-  storage: Storage = localStorage,
+  options?: SyncReducerOptions<State>,
 ): Reducer<State, Args> {
+  const opt = Object.assign(
+    {
+      storage: localStorage,
+      ignore: [],
+      transformFunction: (x: State) => Object.assign(x, opt.ignore.reduce((prev, next) => ({ ...prev, [next]: undefined }), {})),
+    },
+    options
+  );
+
   let mostRecentState: State | undefined = undefined;
 
   window.addEventListener('beforeunload', () => {
-    storage.setItem(storageKey, JSON.stringify(mostRecentState));
+    opt.storage.setItem(
+      storageKey,
+      JSON.stringify(opt.transformFunction(mostRecentState))
+    );
   });
 
   return (state: State, ...args: Args) => {
     // if there is no state...
     if (!state) {
       // ...try to get it from the Storage
-      const item = storage.getItem(storageKey);
+      const item = opt.storage.getItem(storageKey);
       if(item) {
         // Try to parse the JSON string.
         try {
